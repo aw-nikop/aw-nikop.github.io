2c2a5116076ef999b1cd0392eebd7fba6ed0ec04
Niko Permilovsky <nikopermilovsky@gmail.com>
2025-01-24 15:30:06 -0800

    pushing for deploymnet, finished all docs

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,57 +1,55 @@
 import Handlebars from "handlebars";
+
 function getLastUrlPart(url) {
-    const parts = url.split('/');
-    return parts.pop() || parts.pop();  // handle potential trailing slash
+  const parts = url.split("/");
+  return parts.pop() || parts.pop(); // handle potential trailing slash
 }
 
-const data = {
-    "tech-support": {
-        "p1": "Tech Support",
-        "features": [{
-            "image": "bi-cpu",
-            "title": "BI Integration",
-            "content": "Integrate with your BI tools to get the most out of your data."
-        }, 
-        {
-            "image": "bi-cpu",
-            "title": "BI Integration",
-            "content": "Integrate with your BI tools to get the most out of your data."
-        },
-        {
-            "image": "bi-cpu",
-            "title": "BI Integration",
-            "content": "Integrate with your BI tools to get the most out of your data."
-        }
-        ]
-    }
-}
+fetch("/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const url = window.location.href.split("#")[0];
+    const lastPart = getLastUrlPart(url);
+    const template = Handlebars.compile(t);
+    const html = template(data[lastPart]);
+    document.getElementById("solutions-body").innerHTML = html;
+    document.getElementById(
+      "hero"
+    ).style.backgroundImage = `url(${data[lastPart].image})`;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
 const t = `
-<p>
-    {{p1}}
-</p>
 
-<div class="row">
-    {{#each features}}
-    <div class="col">
-        <div class="card">
-            <div class="">
-                <i class="bi {{image}}"></i>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">{{title}}</h5>
-                <p class="card-text">{{content}}</p>
+<div id="hero">
+    <h1> {{title}} </h1>
+    <span class="orange-border"></span>
+</div>
+<section>
+
+    <p>
+        {{p1}}
+    </p>
+
+    <div class="row flex-column flex-sm-row">
+        {{#each features}}
+        <div class="col">
+            <div class="card">
+                <div class="card-icon-container">
+                    <i class="bi {{image}}"></i>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{title}}</h5>
+                    <p class="card-text">{{content}}</p>
+                </div>
             </div>
         </div>
+        {{/each}}
     </div>
-    {{/each}}
-</div>`;
-
-
-const url = window.location.href;
-const lastPart = getLastUrlPart(url);
-console.log(lastPart);
-console.log(data[lastPart]);
-const template = Handlebars.compile(t);
-const html = template(data[lastPart]);
-document.getElementById('body').innerHTML = html;
+    <p>
+        {{p2}}
+    </p>
+</section>
+`;
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,62 +1,65 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import inject from '@rollup/plugin-inject'
-import handlebars from 'vite-plugin-handlebars';
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import inject from "@rollup/plugin-inject";
+import handlebars from "vite-plugin-handlebars";
 
-import { readdirSync } from 'node:fs'
+import { readdirSync } from "node:fs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const SCSS_Logger = {
-    warn(message, options) {
-        // Mute "Mixed Declarations" warning
-        if (options.deprecation && message.includes('mixed-decls')) {
-            return
-        }
-        // List all other warnings
-        console.warn(`▲ [WARNING]: ${message}`);
-    },
+  warn(message, options) {
+    // Mute "Mixed Declarations" warning
+    if (options.deprecation && message.includes("mixed-decls")) {
+      return;
+    }
+    // List all other warnings
+    console.warn(`▲ [WARNING]: ${message}`);
+  },
 };
 
-
-const docsDir = resolve(__dirname, 'src', 'docs')
-const docFiles = readdirSync(docsDir).filter(file => file.endsWith('.html'))
+const docsDir = resolve(__dirname, "src", "docs");
+const docFiles = readdirSync(docsDir).filter((file) => file.endsWith(".html"));
 
 const docInputs = docFiles.reduce((inputs, file) => {
-    const name = file.replace('.html', '')
-    inputs[name] = resolve(docsDir, file)
-    return inputs
-}, {})
+  const name = file.replace(".html", "");
+  inputs[name] = resolve(docsDir, file);
+  return inputs;
+}, {});
 
 export default {
-    plugins: [handlebars({
-        partialDirectory: resolve(__dirname, 'src', 'partials'),
-    })],
-    root: resolve(__dirname, 'src'),
-    resolve: {
-        alias: {
-            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-        }
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src", "partials"),
+    }),
+  ],
+  root: resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
     },
-    server: {
-        port: 8080,
-        hot: true
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src", "index.html"),
+        ...docInputs,
+        data: resolve(__dirname, "src", "public/data.json"),
+        navbar: resolve(__dirname, "src", "partials/navbar.html"),
+        footer: resolve(__dirname, "src", "partials/footer.html"),
+        "thank-you": resolve(__dirname, "src", "thank-you.html"),
+      },
     },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'src', 'index.html'),
-                ...docInputs,
-                navbar: resolve(__dirname, 'src', 'partials/navbar.html'),
-                footer: resolve(__dirname, 'src', 'partials/footer.html'),
-            },
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        logger: SCSS_Logger,
+      },
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                logger: SCSS_Logger,
-            },
-        },
-    },
-}
+  },
+};
