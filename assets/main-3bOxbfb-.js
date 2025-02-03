5d04001b55e520bd8de51a9f112877bfed78efd6
Niko Permilovsky <niko@altonworks.com>
2025-02-03 09:12:27 -0800

    Updates

---
+++ assets/main-3bOxbfb-.js
@@ -0,0 +1,1 @@
+import"./main-DNVsRuru.js";import"./404-BIi7P6kY.js";
--- assets/scroller-DI4MKLUQ.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("DOMContentLoaded",e=>{document.getElementById("main-navbar"),document.getElementById("aw-footer"),console.log(document.getElementById("aw-footer"))});
