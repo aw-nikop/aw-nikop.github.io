362f5ca4991f66f08f41cd2b6a2452e1075c0922
Niko Permilovsky <niko@altonworks.com>
2025-02-04 13:46:53 -0800

    new article

--- src/altons-articles/scroller.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const nav = document.getElementById("main-navbar");
-    const footer = document.getElementById("aw-footer");
-    console.log(document.getElementById("aw-footer"))
-});
