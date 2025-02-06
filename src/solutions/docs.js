5cdd56c6d45be88ca001dd52da5525523c1e2a37
Niko Permilovsky <niko@altonworks.com>
2025-02-06 14:48:30 -0800

    changed solutions to static pages instead

--- src/solutions/docs.js
+++ src/solutions/docs.js
@@ -1,52 +1,52 @@
 import Handlebars from "handlebars";
 
-function getLastUrlPart(url) {
-  const parts = url.split("/");
-  return parts.pop() || parts.pop(); // handle potential trailing slash
-}
+// function getLastUrlPart(url) {
+//   const parts = url.split("/");
+//   return parts.pop() || parts.pop(); // handle potential trailing slash
+// }
 
-fetch("/solutions.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const url = window.location.href.split("#")[0];
-    const lastPart = getLastUrlPart(url);
-    const template = Handlebars.compile(t);
-    const html = template(data[lastPart]);
-    document.getElementById("solutions-body").innerHTML = html;
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+// fetch("/solutions.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const url = window.location.href.split("#")[0];
+//     const lastPart = getLastUrlPart(url);
+//     const template = Handlebars.compile(t);
+//     const html = template(data[lastPart]);
+//     document.getElementById("solutions-body").innerHTML = html;
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
 
-const t = `
+// const t = `
 
-<div id="hero">
-    <h1> {{title}} </h1>
-    <span class="orange-border"></span>
-</div>
-<section>
+// <div id="hero">
+//     <h1> {{title}} </h1>
+//     <span class="orange-border"></span>
+// </div>
+// <section>
 
-    <p>
-        {{p1}}
-    </p>
+//     <p>
+//         {{p1}}
+//     </p>
 
-    <div class="row flex-column flex-sm-row">
-        {{#each features}}
-        <div class="col">
-            <div class="card">
-                <div class="card-icon-container">
-                    <i class="bi {{image}}"></i>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{title}}</h5>
-                    <p class="card-text">{{content}}</p>
-                </div>
-            </div>
-        </div>
-        {{/each}}
-    </div>
-    <p>
-        {{p2}}
-    </p>
-</section>
-`;
+//     <div class="row flex-column flex-sm-row">
+//         {{#each features}}
+//         <div class="col">
+//             <div class="card">
+//                 <div class="card-icon-container">
+//                     <i class="bi {{image}}"></i>
+//                 </div>
+//                 <div class="card-body">
+//                     <h5 class="card-title">{{title}}</h5>
+//                     <p class="card-text">{{content}}</p>
+//                 </div>
+//             </div>
+//         </div>
+//         {{/each}}
+//     </div>
+//     <p>
+//         {{p2}}
+//     </p>
+// </section>
+// `;
