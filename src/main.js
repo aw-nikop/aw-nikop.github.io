6b21e7cf5fe60c6fde44cd03067d2a9d4c2685f0
Niko Permilovsky <niko@altonworks.com>
2025-06-17 16:06:59 -0700

    added mailchimp

--- src/main.js
+++ src/main.js
@@ -5,17 +5,7 @@ import * as bootstrap from 'bootstrap'
 import ls from 'localstorage-slim';
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    const m = new bootstrap.Modal(
-        document.getElementById("signupModal"),
-    );
 
-    if (ls.get("form_shown") === null) {
-
-        setTimeout(() => {
-            m.show();
-            ls.set("form_shown", true, { ttl: 60 * 60 * 24 * 7 });
-        }, 1000 * 20 * 1);
-    }
 
     var canvas = document.getElementById("offcanvasNav");
     var bscanvas = new bootstrap.Offcanvas(canvas);
